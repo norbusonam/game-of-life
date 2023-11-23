e889b25b8c8f7497a45cc44ceca7d6007fb9b261
norbusonam <nsonam1822@gmail.com>
2023-11-23 12:36:04 -0500

    site header

--- src/app.tsx
+++ src/app.tsx
@@ -59,6 +59,9 @@ export function App() {
 
   return (
     <div className="max-w-3xl mx-auto p-4">
+      <h1 className="text-xl font-semibold mb-4 text-center">
+        Conway's Game of Life
+      </h1>
       <Grid width={50} height={50} cells={cells} onCellClick={onCellClick} />
       <div className="flex justify-between">
         <div className="space-x-2">
